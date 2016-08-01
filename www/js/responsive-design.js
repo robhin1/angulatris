bac3a9b92b43527e1f0b5289dda116f018c4064f
Roberto Hinojosa <rhinojosahdz@gmail.com>
2016-08-01 16:15:11 -0400

    done

--- www/js/responsive-design.js
+++ www/js/responsive-design.js
@@ -1,24 +1,71 @@
 setTimeout(function () {
   //@TODO on resize rerun this
   $(function () {
-    var screenWidth = ($(window).width() < window.screen.width) ? $(window).width() : window.screen.width; //get proper width
-    var screenHeight = ($(window).height() < window.screen.height) ? $(window).height() : window.screen.height; //get proper height
-    var pageWidth = 310;//$('#app-left-column').width() + $('#app-right-column').width(); // old phones don't seem to calculate the width correctly using this commented code, so we can run this on the desktop browser and hardcode it for now... same thing for the line under (pageHeight)
-    var pageHeight = 701;//$('#app-all').height(); // min height of site
-    var widthRatio = screenWidth / pageWidth; //calculate ratio
-    var heightRatio = screenHeight / pageHeight; //calculate ratio
-    var minRatio = Math.min(widthRatio, heightRatio);
-    var maxRatio = Math.max(widthRatio, heightRatio);
-    $('#Viewport').attr('content', 'user-scalable=no');
-    var newZoom = (minRatio < 1 || maxRatio < 1) ? minRatio * .90 : maxRatio * .85;
-    //@zone:zoom si el segundo digito de los decimales del zoom termina en un numero impar los bloques de tetris se muestran raros
-    newZoom = /\d.*\.\d\d/.exec(newZoom)[0];
-    if (newZoom[newZoom.length - 1] % 2) {
-      newZoom -= .01;
-    }
-    //\@zone:zoom
-    document.body.style.zoom = newZoom;
 
+    var pageWidth = 312; //$('#app-left-column').width() + $('#app-right-column').width(); // old phones don't seem to calculate the width correctly using this commented code, so we can run this on the desktop browser and hardcode it for now... same thing for the line under (pageHeight)
+    var pageHeight = 700; //$('#app-all').height(); // min height of site
+    var browserAdjustment = .90;
+    var mobileAdjustment = .95;
+
+    var roundDecimalsTo2AndMakeSureItEndsInAEvenNumber = function (oldZoom) {
+      var newZoom;
+      try {
+        newZoom = /\d.*\.\d\d/.exec(oldZoom)[0]
+        if (newZoom[newZoom.length - 1] % 2) {
+          newZoom -= .01;
+        }
+      } catch (e) {
+        newZoom = oldZoom;
+      }
+      return newZoom;
+    }
+    var resizeBrowser = function () {
+      var screenWidth = ($(window).width() < window.screen.width) ? $(window).width() : window.screen.width;
+      var screenHeight = ($(window).height() < window.screen.height) ? $(window).height() : window.screen.height;
+      var widthRatio = screenWidth / pageWidth;
+      var heightRatio = screenHeight / pageHeight;
+      var minRatio = Math.min(widthRatio, heightRatio);
+      var maxRatio = Math.max(widthRatio, heightRatio);
+      var newZoom = .01;
+      while (true) {
+        if (screenWidth < (newZoom * pageWidth) || screenHeight < (newZoom * pageHeight)) {
+          break;
+        }
+        newZoom += .01;
+      }
+      document.body.style.zoom = roundDecimalsTo2AndMakeSureItEndsInAEvenNumber(newZoom * browserAdjustment); //console.log('screenWidth',screenWidth,'screenHeight',screenHeight,'pageWidth',pageWidth,'pageHeight',pageHeight,'widthRatio',widthRatio,'heightRatio',heightRatio,'minRatio',minRatio,'maxRatio',maxRatio);
+    }
+    var resizeMobile = function () {
+      var windowWidth = window.innerWidth,
+        newScaleWidth = windowWidth / pageWidth;
+      var windowHeight = window.innerHeight,
+        newScaleHeight = windowHeight / pageHeight;
+      var newZoom = Math.min(newScaleWidth, newScaleHeight) * mobileAdjustment;
+      document.body.style.zoom = roundDecimalsTo2AndMakeSureItEndsInAEvenNumber(newZoom);
+      var el = $('#app-loading-modal');
+      setTimeout(function () {
+        el.fadeOut('slow', function () {
+          el.remove();
+        });
+      }, 1000);
+    }
+
+    var resizeFinal = function () {
+      if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+        resizeMobile();
+      } else {
+        resizeBrowser();
+      }
+    }
+    resizeFinal();
+    let resizeTimeout;
+    $(window).resize(function () {
+      clearTimeout(resizeTimeout);
+      resizeTimeout = setTimeout(function () {
+        resizeFinal();
+        console.log('resized');
+      }, 1000);
+    });
     var hideLoadingModal = function () {
       var el = $('#app-loading-modal');
       setTimeout(function () {
